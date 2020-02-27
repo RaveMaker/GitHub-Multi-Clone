5383f421e62b5103e06f6286acb607dbc5523c08
RaveMaker <ravemaker@gmail.com>
2020-02-28 01:16:33 +0200

    remake init commit

--- README.md
+++ README.md
@@ -3,19 +3,32 @@ GitHub-Multi-Clone
 
 Clone all User or Organization repositories from GitHub.
 
-Pulls latest update of each repo after clone.
+- Supports both GIT and HTTPS clone.
+- Supports cloning of all repos even if higher then 100 (GitHub api limit).
 
 ### Installation
 
-1. Clone this script from github or copy the files manually to your prefered directory.
+1. Clone this script from github or copy the files manually to your preferred directory.
 
 2. Create settings.cfg from settings.cfg.example and change:
 
-- REPOSNUM="120" - How many repos do you have on this account
-- DESTFOLDER="/folder" - Where to Clone
-- USERORG="1/2" - Select mode - User=1 Organization=2
-- GITNAME="User-or-Organization-Name" - User or Organization Name from GitHub url
+```
+# Where to Clone
+DEST_FOLDER="/d/Dev/repos/TEST"
 
+# User or Organization Name from GitHub url
+GIT_NAME="RaveMaker"
+
+# Select GitHub account type - User=1 Organization=2
+USER_ORG="1"
+
+# Select repo clone mode - GIT=1 HTTPS=2
+GIT_HTTPS="1"
+
+# Debug mode: set to "false" to enable cloning
+DEBUG_MODE="true"
+
+```
 
 Author: [RaveMaker][RaveMaker].
 
--- multiClone.sh
+++ multiClone.sh
@@ -7,72 +7,79 @@
 # GitHub Limitations:
 # For unauthenticated requests, the rate limit allows you to make up to 60 requests per hour.
 
-
 # Load settings
-if [ -f settings.cfg ] ; then
-    echo "Loading settings..."
-    source settings.cfg
-    echo "Cloning Git: " $GITNAME
+if [ -f settings.cfg ]; then
+  echo "Loading settings..."
+  . settings.cfg
+  echo "Cloning Git: $GIT_NAME"
 else
-    echo "ERROR: Create settings.cfg (from settings.cfg.example)"
-    exit
-fi;
-
-# CONST
-REPOSLIST="repos-list.txt"
-GITURLUSER="https://api.github.com/users/"
-GITURLORG="https://api.github.com/orgs/"
-
-# User mode or Org Mode
-if [ $USERORG=="1" ]
-then
-    GITTOCLONE=$GITURLUSER$GITNAME
-elif [ $USERORG=="2" ]
-then
-    GITTOCLONE=$GITURLORG$GITNAME
-else
-    echo "Please select mode"
-    exit;
+  echo "ERROR: Create settings.cfg (from settings.cfg.example)"
+  exit
 fi
 
-# Create repos list and destination folder
-mkdir $DESTFOLDER
-cd $DESTFOLDER
-echo > $REPOSLIST
-echo > $REPOSLIST.2
+# CONSTS
+REPOS_LIST="repos-list.txt"
+GIT_URL_USER="https://api.github.com/users"
+GIT_URL_ORG="https://api.github.com/orgs"
+# GitHub limit of repos in one call 100
+REPO_NUM=100
 
-# Get repos from GitHub
-COUNT=`echo $(( $REPOSNUM / 30 ))`
-let "COUNT += 1"
-COUNTER=1
-while [  $COUNTER -le $COUNT ]; do
-    wget $GITTOCLONE/repos?page=$COUNTER
-    cat repos?page=$COUNTER | grep git_url >> $REPOSLIST.2
-    rm -f repos?page=$COUNTER
-    let "COUNTER += 1"
-done
+# Vars
+GITHUB_PAGE=1
+LAST_PAGE="false"
 
-# Cut repos list to URLs only
+# User mode or Org Mode
+if [ "$USER_ORG" == "1" ]; then
+  GIT_TO_CLONE="$GIT_URL_USER/$GIT_NAME"
+elif [ "$USER_ORG" == "2" ]; then
+  GIT_TO_CLONE="$GIT_URL_ORG/$GIT_NAME"
+else
+  echo "Please select User/Organization mode"
+  exit
+fi
 
-cat $REPOSLIST.2 | sed 's/"git_url": "//' | sed 's/",//' | sed '/^$/d' | sed 's/git:\/\/github.com\///' | sed 's/ //g' | sed "s/$GITNAME\///g" | sed 's/.git//g' > $REPOSLIST
-rm -f $REPOSLIST.2
+function get-repos-list() {
+  # Get repos url list from GitHub
+  echo "Downloading Repo list page $GITHUB_PAGE"
+  if [ "$GIT_HTTPS" == "1" ]; then
+    #get git repos url list
+    curl -s "$GIT_TO_CLONE/repos?per_page=$REPO_NUM&page=$GITHUB_PAGE" | grep -o 'git@[^"]*' >$REPOS_LIST
+  elif [ "$GIT_HTTPS" == "2" ]; then
+    #get https repos url list
+    curl -s "$GIT_TO_CLONE/repos?per_page=$REPO_NUM&page=$GITHUB_PAGE" | grep -w clone_url | grep -o '[^"]\+://.\+.git'>$REPOS_LIST
+  else
+    echo "Please select GIT/HTTPS mode"
+    exit
+  fi
+}
 
 # Clone GitHub repositories from URLs list file
-for REPO in `cat $REPOSLIST`
-do
-   SSHREPO="git@github.com:$GITNAME/$REPO.git"
-   if [ ! -d $REPO ]
-   then
-	git clone $SSHREPO
-   else
-	cd $REPO
-	git pull
-   fi
-   pwd
-   echo "#########################################"
-   cd $DESTFOLDER
+function clone-repos() {
+  while read -r REPO; do
+    echo "Cloning repo: $REPO"
+    if [ "$DEBUG_MODE" == "false" ]; then
+      git clone "$REPO"
+    fi
+  done<"$REPOS_LIST"
+  REPO_COUNT=$(wc -l <"$REPOS_LIST")
+  if ((REPO_COUNT<REPO_NUM)); then
+    LAST_PAGE="true"
+    TOTAL_REPOS=$((REPO_NUM*GITHUB_PAGE+REPO_COUNT-REPO_NUM))
+    echo "Cloned $TOTAL_REPOS"
+  fi
+  ((GITHUB_PAGE+=1))
+}
+
+# Create destination folder if not exist
+mkdir -p "$DEST_FOLDER" && cd "$DEST_FOLDER" || exit
+echo -n "Selected destination: "
+pwd
+
+# Clone repos
+while [ "$LAST_PAGE" == "false" ]; do
+  get-repos-list
+  clone-repos
 done
-echo ""
-echo $GITNAME " repositories count:"
-wc -l $REPOSLIST
-echo ""
+
+# Remove repos list
+rm -f "$REPOS_LIST"
